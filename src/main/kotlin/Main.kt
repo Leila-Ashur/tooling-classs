c991846fbf192d97536b4ac780c3f64fce9f0d15
leilahashur <leilahashur>
2023-02-03 18:33:16 +0300

    first command

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,12 @@
+fun main(args: Array<String>) {
+     var name = "leila"
+    var age = 24
+    var  number = 726794700
+    var wheight = 25
+    var kenyan = false
+    var notkenyan = true
+
+    
+
+
+}
